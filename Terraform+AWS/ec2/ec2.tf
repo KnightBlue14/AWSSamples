576e463073566c7fbc346f45aeeb70f4ffdc68b7
KnightBlue14 <domburton@hotmail.co.uk>
2025-05-28 15:13:03 +0100

    added functions

--- Terraform+AWS/ec2/ec2.tf
+++ Terraform+AWS/ec2/ec2.tf
@@ -7,4 +7,14 @@ provider "aws" {
 resource "aws_instance" "ec2" {
     ami = "ami-0fc32db49bc3bfbb1"
     instance_type = "t2.micro"
+    count = 1
+
+    tags = {
+        name = "terraform_ec2"
+    }
+}
+
+resource "aws_s3_bucket" "c" {
+    bucket = "new_bucket"
+    acl = "private"
 }
